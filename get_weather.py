fc9cc4512de48cef58a891271ce1b2c965696bcb
Sandeep Dcunha <sandeepsbd@yahoo.com>
2024-07-12 15:56:15 -0400

    Added weather api calling

---
+++ get_weather.py
@@ -0,0 +1,11 @@
+import python_weather
+
+import asyncio
+import os
+
+async def get_weather(location):
+  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+    # fetch a weather forecast from a city
+    weather = await client.get(location)
+    return weather
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from nicegui import Tailwind, ui
+import get_weather
+import asyncio
+
 
 class DarkButton(ui.button):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._dark = ui.dark_mode()
+        self._dark = ui.dark_mode() 
         self._state = True
         self.on('click', self.toggle)
         self._dark.enable() if self._state else self._dark.disable()
@@ -23,22 +26,32 @@ class DarkButton(ui.button):
         super().update()
 
 class DailyWeather(ui.card):
-    def __init__(self, date, high, low, *args, **kwargs) -> None:
+    def __init__(self, date=None, high=None, low=None, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.__enter__()
-        ui.label(date)
-        ui.label(high)
-        ui.label(low)
+        self.date_label = ui.label(date)
+        self.high_label = ui.label(high)
+        self.low_label = ui.label(low)
+        self.__exit__()
+    def update(self, date=None, high=None, low=None):
+        self.__enter__()
+        if date:
+            self.date_label.set_text(date)
+        if high:
+            self.high_label.set_text(high)
+        if low:
+            self.low_label.set_text(low)
         self.__exit__()
 
 class HourlyWeather(ui.row):
-    def __init__(self, time, temperature, precipitation=None, *args, **kwargs) -> None:
+    def __init__(self, time=None, temperature=None, precipitation=None, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.__enter__()
         ui.label(time)
         ui.label(temperature)
         ui.label(precipitation)
         self.__exit__()
+
 
 with ui.footer(value=False) as footer:
     ui.label('Footer')
@@ -59,31 +72,51 @@ with ui.header() as header:
     with ui.row().classes('w-full') as header_row_2:
         with ui.row() as location_div:
             ui.label('Location: ')
-            location = ui.label('Madison')
-            set_location_input = ui.input(label='Location')
+            location_label = ui.label('')
+            set_location_input = ui.input(label='Location').on(
+            'blur', lambda e: update_weather(e.value)).on(
+            'keydown.enter', lambda e: location_label.set_text(e.value))
 
 
 with ui.tabs() as tabs:
     ui.tab('Today')
     ui.tab('Hourly')
-    ui.tab('Ten Day')
+    ui.tab('Three Days')
 
 
 with ui.tab_panels(tabs, value='Today').classes('w-full'):
     with ui.tab_panel('Today'):
         with ui.card():
-            ui.label('Today\'s Weather')
+            today_location = ui.label('').classes('h3')
+            ui.label('Temperature')
+            today_temp = ui.label('')
+            ui.label('Humidity')
+            today_humidity = ui.label('')
+            ui.label('Precipitation')
+            today_precipitation = ui.label('')
+            
+            
     with ui.tab_panel('Hourly'):
         for i in range(24):
             HourlyWeather(i, 24)
-    with ui.tab_panel('Ten Day'):
-        with ui.row().classes('no-wrap justify-center') as ten_day_forcast:
-            for i in range(10):
-                DailyWeather('Monday', '72', '12').classes('col')
+    with ui.tab_panel('Three Days'):
+        with ui.row().classes('no-wrap justify-center') as three_day_forcast:
+            three_day_weather_cards = [DailyWeather().classes('col') for i in range(3)]
         with ui.expansion().props('hide-expand-icon') as daily_info_expansion:
             ui.label('Weather Info')
-
-
+            
+            
+    async def update_weather(location):
+        new_weather = await get_weather.get_weather(location)
+        print(new_weather)
+        location_label.set_text(new_weather.location)
+        today_location.set_text(new_weather.location)
+        today_temp.set_text(new_weather.temperature)
+        today_humidity.set_text(new_weather.humidity)
+        today_precipitation.set_text(new_weather.precipitation)
+        for (daily_weather, daily_weather_card) in zip(new_weather.daily_forecasts, three_day_weather_cards):
+            daily_weather_card.update(date=daily_weather.date, high=daily_weather.highest_temperature, low=daily_weather.lowest_temperature)
+    
 
 
 
